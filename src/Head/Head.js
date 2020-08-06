0fac9e6f77623f8096228f486354e68a899dd27a
techeadred <techeadred@icloud.com>
2020-08-06 06:31:47 -0700

    initial commit

---
+++ src/Head/Head.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Head.css';
+
+
+class Head extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>ELF Computing | Laptops</h1>
+            </header>
+        )
+    }
+}
+
+
+export default Head;
